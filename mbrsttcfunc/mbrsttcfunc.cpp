16c5d8e22630d8db124c1e2bc3d1fd2047d9437a
naxsyambis <naxsyambis@gmail.com>
2024-05-14 13:43:26 +0100

    membuat class mahasiswa yang berisi data private yaitu static int nim data public int id, string nama, void setID, void printAll static void setNim, static int getNim dan struck

---
+++ mbrsttcfunc/mbrsttcfunc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// member static function
+
+class mahasiswa {
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+    static int getNim() { return nim; /*Definisi Function*/ }
+
+    mahasiswa(string pnama) :nama(pnama) { setID(); } // :nama(pnama) itu sama aja dengan nama = pnama; 
+};
+
+int main()
+{
+    
+}

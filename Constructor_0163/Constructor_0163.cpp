f7f813f746d4160c7e90b5bf25567fa4ef7662ab
naxsyambis <naxsyambis@gmail.com>
2024-05-14 11:11:47 +0100

    menambahkan nilai nim dengan satu

--- Constructor_0163/Constructor_0163.cpp
+++ Constructor_0163/Constructor_0163.cpp
@@ -20,6 +20,12 @@ public:
 
 int Mahasiswa::nim = 163; // dipanggil kelas baru objeknya, kelas mahasiswa di akses diluar dengan ::
 
+void Mahasiswa::setID() {
+    id = ++nim; // menambahkan plus satu, ditambahkan dulu baru diberikan
+}
+
+
+
 int main()
 {
 

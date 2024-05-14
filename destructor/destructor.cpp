af9f66e98f5f344ca49f68df13adfb93ed543624
naxsyambis <naxsyambis@gmail.com>
2024-05-14 14:16:27 +0100

    cetak data

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -25,6 +25,12 @@ angka::~angka() {
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
 
 
 int main()
